3d82c354c0f15117954a765b198e41286563b7aa
Casey Ochs <caseyo@procore.com>
2018-02-28 09:40:41 -0800

    renamed to csrf_error_reason and cleanup

--- lib/omniauth/strategies/oauth2.rb
+++ lib/omniauth/strategies/oauth2.rb
@@ -68,10 +68,8 @@ module OmniAuth
         if error
           fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
         elsif !options.provider_ignores_state && (request.params["state"].to_s.empty? || request.params["state"] != session["omniauth.state"])
-          session_state = session["omniauth.state"].to_s
-          params_state = request.params["state"].to_s
-          session.delete("omniauth.state")
-          fail!(:csrf_detected, CallbackError.new(:csrf_detected, csrf_error_message(params_state, session_state)))
+          error_reason = csrf_error_reason(request.params["state"].to_s, session.delete("omniauth.state").to_s)
+          fail!(:csrf_detected, CallbackError.new(:csrf_detected, error_reason))
         else
           self.access_token = build_access_token
           self.access_token = access_token.refresh! if access_token.expired?
@@ -87,7 +85,7 @@ module OmniAuth
 
     protected
 
-      def csrf_error_message(params_state, session_state)
+      def csrf_error_reason(params_state, session_state)
         if params_state.empty?
           "CSRF detected -- request.params['state'] is missing"
         elsif session_state.empty?
